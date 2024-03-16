c17cd864dbde09b8458c0303508fb0a1194e97c8
Biancamelare <biancavsmelare@gmail.com>
2024-03-16 15:38:49 -0300

    Configuracoes Iniciais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './home/login/login.component';
+import { CadastroUsuarioComponent } from './home/cadastro-usuario/cadastro-usuario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LoginComponent
+    },
+    {
+        path: 'cadastro',
+        component: CadastroUsuarioComponent
+    }
+];
---
+++ src/app/home/cadastro-usuario/cadastro-usuario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-usuario',
+  standalone: true,
+  imports: [],
+  templateUrl: './cadastro-usuario.component.html',
+  styleUrl: './cadastro-usuario.component.css'
+})
+export class CadastroUsuarioComponent {
+
+}
