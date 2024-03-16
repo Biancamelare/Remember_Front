51f4ff4f3f5d88ed1162ff786b36682d69cf3c81
JenifferFaria <jenifferaparecida10@gmail.com>
2024-03-16 19:03:30 -0300

    incluindo telas home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './home/login/login.component';
 import { CadastroUsuarioComponent } from './home/cadastro-usuario/cadastro-usuario.component';
+import { RedefinirSenhaComponent } from './home/redefinir-senha/redefinir-senha.component';
+import { SenhaComponent } from './home/senha/senha.component';
 
 export const routes: Routes = [
     {
@@ -8,7 +10,19 @@ export const routes: Routes = [
         component: LoginComponent
     },
     {
-        path: 'cadastro',
+        path: 'cadastro-usuario',
         component: CadastroUsuarioComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'redefinir-senha',
+        component: RedefinirSenhaComponent
+    },
+    {
+        path: 'senha',
+        component: SenhaComponent
     }
 ];
---
+++ src/app/home/senha/senha.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-senha',
+  standalone: true,
+  imports: [],
+  templateUrl: './senha.component.html',
+  styleUrl: './senha.component.css'
+})
+export class SenhaComponent {
+
+}
