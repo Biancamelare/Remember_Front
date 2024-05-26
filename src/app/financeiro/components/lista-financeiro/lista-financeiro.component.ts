39bf068b050e727eaa8946c12dec04c0fa4b140d
Biancamelare <biancavsmelare@gmail.com>
2024-05-26 15:30:23 -0300

    componente financeiro

---
+++ src/app/financeiro/components/lista-financeiro/lista-financeiro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-financeiro',
+  standalone: true,
+  imports: [],
+  templateUrl: './lista-financeiro.component.html',
+  styleUrl: './lista-financeiro.component.css'
+})
+export class ListaFinanceiroComponent {
+
+}
---
+++ src/app/financeiro/components/model-financeiro/model-financeiro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-financeiro',
+  standalone: true,
+  imports: [],
+  templateUrl: './model-financeiro.component.html',
+  styleUrl: './model-financeiro.component.css'
+})
+export class ModelFinanceiroComponent {
+
+}
