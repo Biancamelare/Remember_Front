1690b982d0714bbb5873adeb0e0a4f1a4e423dd6
Biancamelare <biancavsmelare@gmail.com>
2024-05-31 14:51:26 -0300

    cor do grafico

--- src/app/shared/models/grafico.ts
+++ src/app/shared/models/grafico.ts
@@ -4,7 +4,7 @@ export const circleProgressOptions: Partial<CircleProgressOptions> = {
   radius: 45,
   outerStrokeWidth: 10,
   innerStrokeWidth: 6,
-  outerStrokeColor: 'var(--primary-color)',
+  outerStrokeColor: 'var(--font-color)',
   innerStrokeColor: 'var(--input-color)',
   animation: true,
   animationDuration: 300,
