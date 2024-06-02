2735acbfd0880eb8a7221f9f9825e188b7d21dfa
Biancamelare <biancavsmelare@gmail.com>
2024-06-02 18:29:45 -0300

    listando finanças

--- src/app/financeiro/components/lista-financeiro/lista-financeiro.component.ts
+++ src/app/financeiro/components/lista-financeiro/lista-financeiro.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { ModelFinanceiroComponent } from '../model-financeiro/model-financeiro.component';
 import { SidenavComponent } from '../../../shared/components/sidenav/sidenav.component';
-import { DOCUMENT, DatePipe } from '@angular/common';
+import { CommonModule, DOCUMENT, DatePipe } from '@angular/common';
 import { AuthServiceService } from '../../../shared/services/auth-service.service';
 import { UsuarioService } from '../../../home/services/usuario.service';
 import { TransacaoService } from '../../services/transacao.service';
@@ -10,11 +10,13 @@ import { UsuarioModel } from '../../../home/models/usuario.model';
 import { PageTransacaoModel } from '../../models/pageTransacao.model';
 import { TransacaoModel } from '../../models/transacao.model';
 import { FuncoesService } from '../../../shared/services/funcoes.service';
+import { AlertasComponent } from '../../../shared/components/alertas/alertas.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-financeiro',
   standalone: true,
-  imports: [SidenavComponent, ModelFinanceiroComponent],
+  imports: [SidenavComponent, ModelFinanceiroComponent, AlertasComponent, CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './lista-financeiro.component.html',
   styleUrl: './lista-financeiro.component.css',
   providers: [DatePipe]
@@ -31,13 +33,18 @@ export class ListaFinanceiroComponent implements OnInit {
   transacao: TransacaoModel[] = [];
   transacoes?: TransacaoModel[]
 
+  entradas: number = 0;
+  saidas: number = 0;
+  saldoAtual: number = 0;
+
+
   constructor(
     @Inject(DOCUMENT) private document: Document,
     private authService: AuthServiceService,
     private usuarioSerive : UsuarioService,
     private transacaoService: TransacaoService,
     private alertaService:AlertaService,
-    private datePipe: DatePipe,
+    public datePipe: DatePipe,
     private funcoesService: FuncoesService
 ) {
   
@@ -70,24 +77,49 @@ export class ListaFinanceiroComponent implements OnInit {
     this.transacaoService.getTransacoes(this.currentUser).subscribe(
       (transacoes: PageTransacaoModel) => {
         this.transacoes = transacoes.data || [];
-        console.log(this.transacoes)
+        this.calcularTotais();
       },
       (error) => {
         this.alertaService.exibirAlerta('danger', 'Erro ao buscar tarefas: ' + error.error.message);
       }
     );
   }
+
+  calcularTotais(): void {
+    this.entradas = this.transacoes
+      ?.filter(transacao => transacao.tipo === 'd' && transacao.preco !== undefined)
+      .reduce((sum, transacao) => sum + (transacao.preco || 0), 0) || 0;
+  
+    this.saidas = this.transacoes
+      ?.filter(transacao => transacao.tipo === 'c' && transacao.preco !== undefined)
+      .reduce((sum, transacao) => sum + (transacao.preco || 0), 0) || 0;
+  
+    this.saldoAtual = this.entradas - this.saidas;
+  }
+
+
+  recarregarTransacoes(): void {
+    this.buscarTransacoes();
+  }
+
+  formatarValor(valor: number | undefined, tipo: string | undefined): string {
+    const valorFormatado = valor?.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    return tipo === 'd' ? `+ ${valorFormatado}` : `- ${valorFormatado}`;
+  }
+
+  tirarVirgula(valor: number | undefined){
+    const valorFormatado = valor?.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    return valorFormatado;
+  }
   
   openModal() {
-    const modalDiv = document.getElementById('exampleModal');
+    const modalDiv = document.getElementById('modalTransacao');
     if(modalDiv!= null) {
       modalDiv.style.display = 'block';
     }
   }
   closeModal() {
-    const modalDiv = document.getElementById('exampleModal');
-    if(modalDiv!= null) {
-      modalDiv.style.display = 'none';
-    }
+
   }
 }
